29b1aefe960b4a48cadffbe2527e049bb3b812e7
HedaetShahriar <shahriahedaet@gmail.com>
2025-03-02 16:37:27 +0600

    Responsive page

--- script/task.js
+++ script/task.js
@@ -18,4 +18,4 @@ const day = date.getDate();
 const month = date.toLocaleString('default', { month: 'short' });
 const year = date.getFullYear();
 
-taskDate.innerHTML = `<h1 class="text-2xl">${weekday}, <br><span class="font-bold ">${month} ${day} ${year}</span></h1>`;
+taskDate.innerHTML = `<h1 class="font-medium md:font-normal lg:text-2xl">${weekday}, <br><span class="font-bold text-lg md:text-xl lg:text-2xl">${month} ${day} ${year}</span></h1>`;
